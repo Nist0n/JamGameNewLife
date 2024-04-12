0ef2ade9b7b7abce3218e6cb86c8472a307b84f5
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-04-13 00:14:13 +0300

    Added tooltips :sparkles:

--- Assets/Scripts/Buildings/Barracks.cs
+++ Assets/Scripts/Buildings/Barracks.cs
@@ -15,12 +15,16 @@ namespace Buildings
         [SerializeField] private Button researchButton; 
         [SerializeField] private TMP_Dropdown racesDropdown;
         [SerializeField] private TMP_Dropdown unitsDropdown;
+        [SerializeField] private TMP_Dropdown researchRacesDropdown;
+        [SerializeField] private TMP_Dropdown researchUnitsDropdown;
         
         [SerializeField] private Slider unitsCountSlider;
         [SerializeField] private TMP_Text unitsCountText;
         [SerializeField] private TMP_Text coinsCountText;
         [SerializeField] private TMP_Text addedCoinsText;
         [SerializeField] private TMP_Text leadershipCountText;
+        [SerializeField] private TMP_Text sumText;
+        // [SerializeField] private TMP_Text leadershipCheckoutText;
 
         [SerializeField] private List<TMP_Dropdown.OptionData> humanOptions = new();
         [SerializeField] private List<TMP_Dropdown.OptionData> gnomeOptions = new();
@@ -29,11 +33,15 @@ namespace Buildings
         [SerializeField] private GameObject addUnitControls;
 
         [SerializeField] private Button acceptButton;
+        
+        [SerializeField] private Button addUnitButton;
+        [SerializeField] private Button removeUnitButton;
 
         private OurHand _ourHand;
 
         private bool _controlsShown;
         private bool _racesShown;
+        private bool _researchRacesShown;
 
         private string _selectedUnit = "Peasant";
 
@@ -43,10 +51,13 @@ namespace Buildings
         private int _unitCoinsCost = 10;
         private int _maxUnitsOfType;
 
+        private Dictionary<string, int> _units = new();
+        
         private void Start()
         {
             _ourHand = FindObjectOfType<OurHand>();
             _leadership = PlayerPrefs.GetInt(PlayerData.LeadershipData);
+            // TODO: change leadership according to saved units data
             _maxUnitsOfType = _leadership / 5;
         }
 
@@ -56,10 +67,43 @@ namespace Buildings
             _leadership = Convert.ToInt32(leadershipCountText.text);
 
             unitsCountSlider.maxValue = Math.Min(_maxUnitsOfType, _coins / _unitCoinsCost);
+
+            addUnitButton.interactable = unitsCountSlider.value != unitsCountSlider.maxValue;
+            removeUnitButton.interactable = unitsCountSlider.value != unitsCountSlider.minValue;
+            unitsCountSlider.interactable = unitsCountSlider.maxValue != 0;
             
             unitsCountText.text = unitsCountSlider.value.ToString(CultureInfo.CurrentCulture);
             
             acceptButton.gameObject.SetActive(unitsCountSlider.value != 0);
+
+            sumText.text = (unitsCountSlider.value * _unitCoinsCost).ToString(CultureInfo.InvariantCulture);
+            // leadershipCheckoutText.text = (unitsCountSlider.value * _unitCoinsCost).ToString(CultureInfo.InvariantCulture);
+            
+            switch (_selectedUnit)
+            {
+                case "Knight":
+                    _unitCoinsCost = 70;
+                    _maxUnitsOfType = _leadership / 35;
+                    break;
+                case "Archer":
+                case "Mage":
+                    _unitCoinsCost = 100;
+                    _maxUnitsOfType = _leadership / 50;
+                    break;
+                case "Horseman":
+                    _unitCoinsCost = 800;
+                    _maxUnitsOfType = _leadership / 180;
+                    break;
+                default:
+                    _unitCoinsCost = 10;
+                    _maxUnitsOfType = _leadership / 5;
+                    break;
+            }
+
+            if (_units.TryGetValue(_selectedUnit, out var unit))
+            {
+                _maxUnitsOfType -= unit;
+            }
         }
         
         public void ToggleControls()
@@ -71,6 +115,8 @@ namespace Buildings
             _racesShown = false;
             racesDropdown.gameObject.SetActive(false);
             unitsDropdown.gameObject.SetActive(false);
+            researchRacesDropdown.gameObject.SetActive(false);
+            researchUnitsDropdown.gameObject.SetActive(false);
             addUnitControls.gameObject.SetActive(false);
         }
 
@@ -112,40 +158,26 @@ namespace Buildings
         {
             int pickedEntryIndex = unitsDropdown.value;
             _selectedUnit = unitsDropdown.options[pickedEntryIndex].text;
-            
-            switch (_selectedUnit)
-            {
-                case "Knight":
-                    _unitCoinsCost = 70;
-                    _maxUnitsOfType = _leadership / 35;
-                    break;
-                case "Archer":
-                case "Mage":
-                    _unitCoinsCost = 100;
-                    _maxUnitsOfType = _leadership / 50;
-                    break;
-                case "Horseman":
-                    _unitCoinsCost = 800;
-                    _maxUnitsOfType = _leadership / 180;
-                    break;
-                default:
-                    _unitCoinsCost = 10;
-                    _maxUnitsOfType = _leadership / 5;
-                    break;
-            }
         }
 
         public void TrainUnits()
         {
-            List<string> units = new();
+            // List<string> units = new();
             int count = Convert.ToInt32(unitsCountSlider.value);
 
-            for (int i = 0; i < count; i++)
+            if (_units.ContainsKey(_selectedUnit))
             {
-                units.Add(_selectedUnit);
-                _ourHand.AddCountOfUnits(_selectedUnit, count);
-                Debug.Log(units[i]);
+                _units[_selectedUnit] += count;
             }
+            else
+            {
+                _units.Add(_selectedUnit, count);  
+            }
+            // for (int i = 0; i < count; i++)
+            // {
+            //     _ourHand.AddCountOfUnits(_selectedUnit, count);
+            //     Debug.Log(units[i]);
+            // }
 
             int coinsSpent = _unitCoinsCost * count;
             _coins -= coinsSpent;
@@ -173,6 +205,36 @@ namespace Buildings
             yield return new WaitForSeconds(3f);
             
             addedCoinsText.gameObject.SetActive(false);
+        }
+
+        public void ToggleResearchDropdown()
+        {
+            _researchRacesShown = !_researchRacesShown;
+            researchRacesDropdown.gameObject.SetActive(_researchRacesShown);
+            
+            researchUnitsDropdown.gameObject.SetActive(_researchRacesShown);
+        }
+        
+        public void SelectResearchRace()
+        {
+            int pickedEntryIndex = researchRacesDropdown.value;
+            string selectedRace = researchRacesDropdown.options[pickedEntryIndex].text;
+            
+            researchUnitsDropdown.ClearOptions();
+            switch (selectedRace)
+            {
+                case "Люди":
+                    researchUnitsDropdown.AddOptions(humanOptions);
+                    break;
+                case "Гномы":
+                    researchUnitsDropdown.AddOptions(gnomeOptions);
+                    break;
+                case "Орки":
+                    researchUnitsDropdown.AddOptions(orcOptions);
+                    break;
+            }
+            
+            researchUnitsDropdown.gameObject.SetActive(true);
         }
     }
 }
