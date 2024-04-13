074e3fd7b0b9be74a06c4cbf4bae773695231add
IDidThisShit <krupinvlad.2005@gmail.com>
2024-04-13 18:37:33 +0300

    Типа звуки настроил

--- Assets/Scripts/Audio/VolumeSettings.cs
+++ Assets/Scripts/Audio/VolumeSettings.cs
@@ -7,14 +7,37 @@ using UnityEngine.UI;
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer _myMixer;
-    [SerializeField] private Slider _Slider;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _SFXSlider;
     private void Start()
     {
-        SetAllVolume();
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
     }
-    public void SetAllVolume()
+    public void SetMusicVolume()
     {
-        float volume = _Slider.value;
-        _myMixer.SetFloat("Volume", MathF.Log10(volume)*20);
+        float volume = _musicSlider.value;
+        _myMixer.SetFloat("music", MathF.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        float volume = _SFXSlider.value;
+        _myMixer.SetFloat("SFX", MathF.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+    private void LoadVolume()
+    {
+        _musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        _SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        SetMusicVolume();
+        SetSFXVolume();
     }
 }
