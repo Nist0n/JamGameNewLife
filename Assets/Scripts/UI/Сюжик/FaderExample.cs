c0781f17fc269894da4d9de5f27ab6dde69df265
Niston <vadimpavlov@bk.ru>
2024-04-14 00:37:28 +0300

    Fader

---
+++ Assets/Scripts/UI/Сюжик/FaderExample.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FaderExample : MonoBehaviour
+{
+    private bool _isLoading;
+
+    private static FaderExample _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) LoadScene("Base");
+        if (Input.GetKeyDown(KeyCode.Alpha1)) LoadScene("MainMenu");
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        if (_isLoading)
+        {
+            return;
+        }
+
+        var currentSceneName = SceneManager.GetActiveScene().name;
+        if (currentSceneName == sceneName)
+        {
+            
+        }
+
+        StartCoroutine(LoadSceneRoutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneRoutine(string sceneName)
+    {
+        _isLoading = true;
+
+        var waitFading = true;
+        Fade.instance.FadeIn(() => waitFading = false);
+
+        while (waitFading) yield return null;
+
+        var async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+
+        while (async.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        async.allowSceneActivation = true;
+
+        waitFading = true;
+        Fade.instance.FadeOut(() => waitFading = false);
+
+        while (waitFading)
+        {
+            yield return null;
+        }
+
+        _isLoading = false;
+    }
+}
