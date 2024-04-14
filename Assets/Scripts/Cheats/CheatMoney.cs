0281af4bfa95f3439894bc78bfdc4666555bd6c4
Niston <vadimpavlov@bk.ru>
2024-04-14 21:16:48 +0300

    Update CheatMoney.cs

--- Assets/Scripts/Cheats/CheatMoney.cs
+++ Assets/Scripts/Cheats/CheatMoney.cs
@@ -8,8 +8,7 @@ public class CheatMoney : MonoBehaviour
     private bool _actice = false;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.N) && Input.GetKeyDown(KeyCode.E) && Input.GetKeyDown(KeyCode.G) &&
-            Input.GetKeyDown(KeyCode.R) && !_actice)
+        if (Input.GetKeyDown(KeyCode.N) && !_actice)
         {
             PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 100000);
             _actice = true;
